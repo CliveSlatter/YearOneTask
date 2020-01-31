a5665b86ab29ebbd96f20241b627adde57178a3c
00172 <PR.56Ome69th.40EUS>
2020-01-31 14:39:17 +0000

    Actors list added and corrected the search bar. Search bar doesn't work.

--- movies.js
+++ movies.js
@@ -11,9 +11,13 @@ function listMovies(){
             alert(movies.error);
         } else if (movies.hasOwnProperty('moviesList') && movies.moviesList.length > 0) {
             movieHTML+=     `<tr>`+
+                            `<th colspan="4" style="text-align: center">Movie List</th>`+
+                            `</tr>`+
+                            `<tr>`+
                             `<th>Title</th>`+
                             `<th>Rating</th>`+
                             `<th>Studio</th>`+
+                            `<th>Release Year</th>`+
                             `</tr>`;
 
             for (let movie of movies.moviesList) {
@@ -21,8 +25,9 @@ function listMovies(){
                              `<td>${movie.title}</td>`+
                             `<td>${movie.rating}</td>`+
                             `<td>${movie.studio}</td>`+
+                            `<td>${movie.releaseYear}</td>`+
                             `<td>`+
-                            `<button id="delete">Delete movie</button>`+
+                            `<button id="delete"+${movie.id}>Delete movie</button>`+
                             `</td>`+
                             `</td>`+
                             `</tr>`;
@@ -31,21 +36,40 @@ function listMovies(){
             document.getElementById('movie').innerHTML = movieHTML;
 
             let tableRows = document.getElementsByClassName('tableRows');
+            let idx = tableRows.rowIndex;
+
             for(let tr of tableRows){
-                tr.addEventListener('dblclick',showDetails);
+                tr.addEventListener('dblclick',function (){showDetails(this.id)},false);
             }
-
         }
-
     });
 }
 
-function showDetails(){
-
-    $("#movies tr").dblclick(function(){
-        $(this).addClass('selected').siblings().removeClass('selected');
-        let value=$(this).find('td:first').html();
-        alert(value);
-    });
-
+function showDetails(idx){
+    let characterHTML = ``;
+        fetch('/actor/movies/'+idx,{method: 'get'})
+            .then(actorList=>actorList.json())
+            .then(actors=>{
+                console.log(actors.toString())
+            if(actors.hasOwnProperty('error')){
+                alert(actors.error);
+            }else if(actors.hasOwnProperty('movieActors')){
+                characterHTML += `<table>`+
+                                    `<tr>`+
+                                    `<th colspan="2" style="text-align: center">Cast List</th>`+
+                                    `</tr>`+
+                                    `<tr>`+
+                                    `<th>Actor</th>`+
+                                    `<th>Character</th>`+
+                                    `</tr>`;
+                for(let actor of actors.movieActors){
+                    characterHTML+= `<tr class="unselectable" id="${actor.id}">`+
+                                    `<td>${actor.firstName}`+" "+`${actor.surname}</td>`+
+                                    `<td>${actor.character}</td>`+
+                                    `</tr>`;
+                }
+                characterHTML += `</table>`;
+                document.getElementById('info').innerHTML=characterHTML;
+            }
+        })
 }
